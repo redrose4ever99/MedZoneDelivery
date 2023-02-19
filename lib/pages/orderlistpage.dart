e654df6ebef6f884d2e6044b68be6ed63ee52a26
redrose4ever99 <redrose4ever99@gmail.com>
2023-02-19 12:01:17 +0300

    after add inkwell

--- lib/pages/orderlistpage.dart
+++ lib/pages/orderlistpage.dart
@@ -55,169 +55,174 @@ class Orderlist extends StatelessWidget {
               itemCount: 23,
               scrollDirection: Axis.vertical,
               itemBuilder: (BuildContext context, int index) {
-                return  Container(
-                  padding: getPadding(
-                    all: 15,
-                  ),
-                  decoration: AppDecoration.outlineBluegray501.copyWith(
-                    borderRadius: BorderRadiusStyle.roundedBorder8,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: getPadding(
-                          right: 8,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: getPadding(
-                                bottom: 2,
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                  "irehrfhjeb",
-                                    overflow: TextOverflow.ellipsis,
-                                    textAlign: TextAlign.right,
-                                    style: AppStyle.txtInterSemiBold18,
-                                  ),
-                                  Padding(
-                                    padding: getPadding(
-                                      top: 2,
+                return InkWell(
+                  // When the user taps the button, show a snackbar.
+                  onTap: () { onTapNotDelivery(context);
+                  },
+                  child: Container(
+                    padding: getPadding(
+                      all: 15,
+                    ),
+                    decoration: AppDecoration.outlineBluegray501.copyWith(
+                      borderRadius: BorderRadiusStyle.roundedBorder8,
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: getPadding(
+                            right: 8,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: getPadding(
+                                  bottom: 2,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                    "irehrfhjeb",
+                                      overflow: TextOverflow.ellipsis,
+                                      textAlign: TextAlign.right,
+                                      style: AppStyle.txtInterSemiBold18,
                                     ),
-                                    child: Row(
-                                      children: [
-                                        CustomImageView(
-                                          svgPath: ImageConstant.imgLocation,
-                                          height: getSize(
-                                            15.00,
+                                    Padding(
+                                      padding: getPadding(
+                                        top: 2,
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          CustomImageView(
+                                            svgPath: ImageConstant.imgLocation,
+                                            height: getSize(
+                                              15.00,
+                                            ),
+                                            width: getSize(
+                                              15.00,
+                                            ),
                                           ),
-                                          width: getSize(
-                                            15.00,
+                                          Text(
+                                            "جرمانا الجناين",
+                                            overflow: TextOverflow.ellipsis,
+                                            textAlign: TextAlign.right,
+                                            style: AppStyle.txtInterMedium12Gray700,
                                           ),
-                                        ),
-                                        Text(
-                                          "جرمانا الجناين",
-                                          overflow: TextOverflow.ellipsis,
-                                          textAlign: TextAlign.right,
-                                          style: AppStyle.txtInterMedium12Gray700,
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: getPadding(
-                          top: 1,
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            CustomImageView(
-                              svgPath: ImageConstant.imgCalendar15x15,
-                              height: getSize(
-                                10.00,
+                        Padding(
+                          padding: getPadding(
+                            top: 1,
+                          ),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CustomImageView(
+                                svgPath: ImageConstant.imgCalendar15x15,
+                                height: getSize(
+                                  10.00,
+                                ),
+                                width: getSize(
+                                  10.00,
+                                ),
                               ),
-                              width: getSize(
-                                10.00,
+                              Padding(
+                                padding: getPadding(
+                                  left: 5,
+                                ),
+                                child: Text(
+                                  "26/06/2021",
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: AppStyle.txtInterMedium12Gray700,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: getPadding(
-                                left: 5,
+                              CustomImageView(
+                                svgPath: ImageConstant.imgClock15x15,
+                                height: getSize(
+                                  10.00,
+                                ),
+                                width: getSize(
+                                  10.00,
+                                ),
+                                margin: getMargin(
+                                  left: 14,
+                                ),
                               ),
-                              child: Text(
-                                "26/06/2021",
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.left,
-                                style: AppStyle.txtInterMedium12Gray700,
+                              Padding(
+                                padding: getPadding(
+                                  left: 5,
+                                ),
+                                child: Text(
+                                  "10:30 AM",
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: AppStyle.txtInterMedium12Gray700,
+                                ),
                               ),
-                            ),
-                            CustomImageView(
-                              svgPath: ImageConstant.imgClock15x15,
-                              height: getSize(
-                                10.00,
-                              ),
-                              width: getSize(
-                                10.00,
-                              ),
-                              margin: getMargin(
-                                left: 14,
-                              ),
-                            ),
-                            Padding(
-                              padding: getPadding(
-                                left: 5,
-                              ),
-                              child: Text(
-                                "10:30 AM",
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.left,
-                                style: AppStyle.txtInterMedium12Gray700,
-                              ),
-                            ),
-                            Container(
-                              height: getSize(
-                                4.00,
-                              ),
-                              width: getSize(
-                                4.00,
-                              ),
-                              margin: getMargin(
-                                left: 12,
-                                top: 2,
-                                bottom: 2,
-                              ),
-                              decoration: BoxDecoration(
-                                color: ColorConstant.green300,
-                                borderRadius: BorderRadius.circular(
-                                  getHorizontalSize(
-                                    3.00,
+                              Container(
+                                height: getSize(
+                                  4.00,
+                                ),
+                                width: getSize(
+                                  4.00,
+                                ),
+                                margin: getMargin(
+                                  left: 12,
+                                  top: 2,
+                                  bottom: 2,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: ColorConstant.green300,
+                                  borderRadius: BorderRadius.circular(
+                                    getHorizontalSize(
+                                      3.00,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: getPadding(
-                                left: 5,
+                              Padding(
+                                padding: getPadding(
+                                  left: 5,
+                                ),
+                                child: Text(
+                                  "بانتظار التسليم",
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: AppStyle.txtInterMedium12Gray700,
+                                ),
                               ),
-                              child: Text(
-                                "بانتظار التسليم",
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.left,
-                                style: AppStyle.txtInterMedium12Gray700,
-                              ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: getPadding(
-                          top: 7,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CustomButton(height: 50, width: 150, text: "تم التسليم",onTap:() => onTapDelivery(context) ,),
-                            CustomButton(height: 50, width: 150, text: "تعذر التسليم ",onTap:()=> onTapNotDelivery(context) ,),
+                        Padding(
+                          padding: getPadding(
+                            top: 7,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CustomButton(height: 50, width: 150, text: "تم التسليم",onTap:() => onTapDelivery(context) ,),
+                              CustomButton(height: 50, width: 150, text: "تعذر التسليم ",onTap:()=> onTapNotDelivery(context) ,),
 
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 );
               }),
